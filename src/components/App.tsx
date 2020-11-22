8982d707f747cbc4f82fa96ab0a86a815abb4245
Sam Leung <sam_leung@sam.leung>
2020-11-22 16:05:35 +0800

    init

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,70 +1,18 @@
-import React, { FC, ReactElement } from "react";
-import styled from "@emotion/styled";
-import { useTheme } from "./../contexts/ThemeContext";
-import { themeContentInterface } from "./../styles/theme";
-// import { css } from '@emotion/react'
+import React, { useState } from "react";
 
-// type CssType = typeof css
+import DarkModeSwitch from "./DarkModeSwitch";
 
-type StyleProps = {
-  theme?: themeContentInterface;
-};
-
-// export interface IStyleFunction<P = {}> {
-//   ( arg0: { theme: themeContentInterface } & P): ReturnType<CssType> | false | null
-// }
-
-const Wrapper = styled.div<StyleProps>`
-  background: ${({ theme }) => theme.background};
-  width: 95vw;
-  height: 95vh;
-  margin: auto;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen";
-  transition: background 1000ms linear, color 1000ms linear;
-  h1 {
-    color: ${({ theme }) => theme.body};
-  }
-`;
-
-// interface Props {
-//   children: JSX.Element[] | JSX.Element
-// }
-
-const App: FC = () => {
-  const themeState = useTheme();
-
-  /**
-   * Title
-   */
-  const renderTitle = (): ReactElement => {
-    const content: string = themeState.dark ? "DarkMode" : "Light Mode";
-
-    return <h1>{content}</h1>;
-  }
-
-  /**
-   * Content
-   */
-
-  const renderContent = (): ReactElement => {
-    return (
-      <div>
-        <button onClick={() => themeState.toggle()}>
-          {themeState.dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
-        </button>
-      </div>
-    );
+const App = () => {
+  const [mode, setMode] = useState(false);
+  const onChange = () => {
+    setMode(!mode);
   }
 
   return (
-    <Wrapper>
-      {/* Title */}
-      {renderTitle()}
-
-      {/* Content */}
-      {renderContent()}
-    </Wrapper>
+    <div style={{ background: "yellowgreen" }}>
+      <DarkModeSwitch onChange={onChange} checked={mode} />
+    </div>
   );
-};
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { ThemeProvider } from "./contexts/ThemeContext";
 import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.querySelector("#root"),
 );
